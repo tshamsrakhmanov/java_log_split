54a5b566f7b758c4e707e34441be75c41417e625
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-24 15:48:54 +0400

    init commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Argument count: " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+
+        // check if arguments provided
+        // (log-file path) (sample name of part_log_file prompt as PART_) (how many parts: 5-10, basic = 5)
+        // if not - show ERROR prompt, exit program
+        // got to log-file, check existence
+        // if not existing OR empty - ERROR prompt
+        // estimate file size and calculate batch size (based on 3rd flag)
+
+        // cycle:
+        // go line-by-line in log file
+        // if
+
+        //
+
+    }
+}
