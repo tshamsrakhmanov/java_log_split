b25fd7aaed12174a6e18f2928774b189ae5b42fc
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-24 17:23:57 +0400

    help rework

--- Main.java
+++ Main.java
@@ -17,17 +17,18 @@ public class Main {
 
         // check if 1st flag is help -> provide it
         if (args[0].equals("--help") || args[0].equals("-help")) {
-            System.out.println("\njava_log_split v0.05");
+            System.out.println("java_log_split v0.05");
             System.out.println("Usage: CMD [PATH1] [PATH2] [SPLIT]");
-            System.out.println("PATH1");
+            System.out.println("PATH1 {mandatory argument}");
             System.out.println("    Path to log file which is taken for splitting operation");
-            System.out.println("PATH2");
+            System.out.println("PATH2 {mandatory argument}");
             System.out.println("    Path to folder, where split log files will be reside");
-            System.out.println("SPLIT");
+            System.out.println("SPLIT {optional argument}");
             System.out.println("    Number of pieces to split log file to");
+            System.out.println("    base value: 5");
             System.out.println(" ");
-            System.out.println("    // Author: Timur Shamsrakhmanov //");
-            System.out.println("    // Autumn 2024                  //");
+            System.out.println("// Author: Timur Shamsrakhmanov //");
+            System.out.println("// Autumn 2024                  //");
             return;
         }
 
@@ -35,12 +36,14 @@ public class Main {
         if (args.length >= 2) {
             System.out.println("Arguments provided. Go with program...");
         } else {
-            System.out.println("Not enough arguments provided. Abort.");
-            System.out.println("Press ENTER to exit.");
+            System.out.printf("// java_log_splitter ver.%s\n",VERSION);
+            System.out.println("// Not enough arguments provided. Abort.");
+            System.out.println("// Please see --help or -help for instructions");
+            System.out.println("// Press ENTER to exit.");
+
             String _ = input.nextLine();
             return;
         }
-
 
         System.out.println("------------------------------------");
         System.out.println("PROGRAM EXECUTION");
