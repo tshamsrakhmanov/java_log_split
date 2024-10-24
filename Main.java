50260f4f8e02bcce870861296ff6d804ec397bd5
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-24 16:16:35 +0400

    commit

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
 
         System.out.println("Argument count: " + args.length);
         for (int i = 0; i < args.length; i++) {
             System.out.println("Argument " + i + ": " + args[i]);
         }
+
+        // if args not provided -> exit program
+        if (args.length >= 2) {
+            System.out.println("Arguments provided. Go with program...");
+        } else {
+            System.out.println("Not enough arguments provided. Abort.");
+            System.out.println("Press ENTER to exit.");
+            String some_input = input.nextLine();
+        }
+
+
+        // TODO check if 1st flag is --help, then provide info and exit
+
 
         // check if arguments provided
         // (log-file path) (sample name of part_log_file prompt as PART_) (how many parts: 5-10, basic = 5)
