a9d97e5a1e0b981d62768c0bb2171f45ac67c074
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-25 09:23:06 +0400

    sample logger divided into separate class

--- Main.java
+++ Main.java
@@ -1,37 +1,26 @@
+
+
 public class Main {
     public static void main(String[] args) {
 
         final String VERSION = "0.05";
 
+        SampleLogger sampleLogger = new SampleLogger();
+
         if (args.length < 1) {
-            System.out.printf("// java_log_splitter ver.%s\n",VERSION);
-            System.out.println("// No valid arguments provided");
-            System.out.println("// Please see --help or -help for instructions");
+            sampleLogger.error_no_valid_arguments(VERSION);
             return;
         }
 
         // check if 1st flag is help -> provide it
         if (args[0].equals("--help") || args[0].equals("-help")) {
-            System.out.println("java_log_split v0.05");
-            System.out.println("Usage: CMD [PATH1] [PATH2] [SPLIT]");
-            System.out.println("PATH1 {mandatory argument}");
-            System.out.println("    Path to log file which is taken for splitting operation");
-            System.out.println("PATH2 {mandatory argument}");
-            System.out.println("    Path to folder, where split log files will be reside");
-            System.out.println("SPLIT {optional argument}");
-            System.out.println("    Number of pieces to split log file to");
-            System.out.println("    base value: 5");
-            System.out.println(" ");
-            System.out.println("// Author: Timur Shamsrakhmanov //");
-            System.out.println("// Autumn 2024                  //");
+            sampleLogger.info_help_message(VERSION);
             return;
         }
 
         // if not all args not provided -> exit program
         if (args.length < 2) {
-            System.out.printf("// java_log_splitter ver.%s\n",VERSION);
-            System.out.println("// Not enough arguments provided. Abort.");
-            System.out.println("// Please see --help or -help for instructions");
+            sampleLogger.error_not_enough_arguments(VERSION);
             return;
         }
 
---
+++ SampleLogger.java
@@ -0,0 +1,34 @@
+
+
+public class SampleLogger {
+    public void error_no_valid_arguments(String version) {
+        System.out.printf("// java_log_splitter ver. %s\n", version);
+        System.out.println("// No valid arguments provided");
+        System.out.println("// Please see --help or -help for instructions");
+    }
+
+    public void info_help_message(String version) {
+        System.out.printf("java_log_split ver. %s\n", version);
+        System.out.println("Usage: CMD [PATH1] [PATH2] [SPLIT]");
+        System.out.println("PATH1 {mandatory argument}");
+        System.out.println("    Path to log file which is taken for splitting operation");
+        System.out.println("PATH2 {mandatory argument}");
+        System.out.println("    Path to folder, where split log files will be reside");
+        System.out.println("SPLIT {optional argument}");
+        System.out.println("    Number of pieces to split log file to");
+        System.out.println("    base value: 5");
+        System.out.println(" ");
+        System.out.println("// Author: Timur Shamsrakhmanov //");
+        System.out.println("// Autumn 2024                  //");
+    }
+
+    public void error_not_enough_arguments(String version) {
+        System.out.printf("// java_log_splitter ver. %s\n", version);
+        System.out.println("// Not enough arguments provided. Abort.");
+        System.out.println("// Please see --help or -help for instructions");
+    }
+
+//    public void
+
+
+}
