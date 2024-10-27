496c742e4f0b55edbe316cc153ef453e5a687115
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-27 13:44:32 +0400

    logger and help update

--- README.md
+++ README.md
@@ -4,16 +4,19 @@
 
 ```
 JAVA_LOG_SPLIT
-
 Usage: java -jar <jar filename> [PATH1] [PATH2] [SPLIT]
-PATH1 {mandatory argument}
+[PATH1]     {mandatory}
     Path to log file which is taken for splitting operation
-PATH2 {mandatory argument}
+[PATH2]     {mandatory}
     Path to folder + template name of a split. Ex.: C:/<name template>
     By this in 'C:/' will be created <name template>1, <name template>2, ... etc
     Standard notation of pieces is 1, 2, 3, ... etc
     Use /normal/ slash in path declaration
-SPLIT {optional argument}
+[SPLIT]     {optional}
     Number of pieces to split log file to
     base value: 5
+            please don't try more than 10, facilitation of such files are not so good :(
+ 
+// Author: Timur Shamsrakhmanov //
+// Autumn 2024                  //
 ```
--- SampleLogger.java
+++ SampleLogger.java
@@ -7,23 +7,22 @@ public class SampleLogger {
     }
 
     public void info_help_message() {
-        System.out.println(" ");
         System.out.println("JAVA_LOG_SPLIT");
-        System.out.println("Usage: CMD [PATH1] [PATH2] [SPLIT]");
-        System.out.println("PATH1 {mandatory argument}");
+        System.out.println("Usage: java -jar <jar filename> [PATH1] [PATH2] [SPLIT]");
+        System.out.println("[PATH1]     {mandatory}");
         System.out.println("    Path to log file which is taken for splitting operation");
-        System.out.println("PATH2 {mandatory argument}");
+        System.out.println("[PATH2]     {mandatory}");
         System.out.println("    Path to folder + template name of a split. Ex.: C:/<name template>");
         System.out.println("    By this in 'C:/' will be created <name template>1, <name template>2, ... etc");
         System.out.println("    Standard notation of pieces is 1, 2, 3, ... etc");
         System.out.println("    Use /normal/ slash in path declaration");
-        System.out.println("SPLIT {optional argument}");
+        System.out.println("[SPLIT]     {optional}");
         System.out.println("    Number of pieces to split log file to");
         System.out.println("    base value: 5");
+        System.out.println("            please don't try more than 10, facilitation of such files are not so good :(");
         System.out.println(" ");
         System.out.println("// Author: Timur Shamsrakhmanov //");
         System.out.println("// Autumn 2024                  //");
-        System.out.println(" ");
     }
 
     public void error_not_enough_arguments() {
